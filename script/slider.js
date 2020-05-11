e8b51ad265f1fce4f438350a1711e6e92193a29d
takumikunn15 <takumikunn15@gmail.com>
2020-05-11 14:52:13 +0900

    jsをcssで上書きしてたのでなおした

--- script/slider.js
+++ script/slider.js
@@ -1,130 +1,63 @@
-$header-height: 70px;
-
-* {
-  box-sizing: border-box;
+var elem = document.getElementsByClassName("range-red");
+var rangeValue = function (elem, target) {
+	return function (evt) {
+		// target.innerHTML = elem.value;
+    let test = elem.value;
+    colorChange("red", test);
+	};
+};
+for (var i = 0, max = elem.length; i < max; i++) {
+	bar = elem[i].getElementsByTagName("input")[0];
+	target = elem[i].getElementsByTagName("p")[0];
+	bar.addEventListener("input", rangeValue(bar, target));
 }
 
-body {
-  header {
-    background-color: #656565;
-    color: #fff;
+var elem = document.getElementsByClassName("range-green");
+var rangeValue = function (elem, target) {
+	return function (evt) {
+		// target.innerHTML = elem.value;
+    let test = elem.value;
+    colorChange("green", test);
+	};
+};
+for (var i = 0, max = elem.length; i < max; i++) {
+	bar = elem[i].getElementsByTagName("input")[0];
+	target = elem[i].getElementsByTagName("p")[0];
+	bar.addEventListener("input", rangeValue(bar, target));
+}
 
-    .container {
-      margin: 0 auto;
-      width: 80%;
-      height: $header-height;
-
-      .header-left {
-        display: inline-block;
-        float: left;
-        font-size: 30px;
-        line-height: $header-height;
-      }
-
-      .header-right p,
-      a {
-        display: inline-block;
-        float: right;
-        color: #fff;
-        text-decoration: none;
-        font-size: 20px;
-        line-height: $header-height;
-
-        a {
-          padding: 0 20px;
-          background-color: rgba(#fff,.3);
-
-          &:hover {
-            background-color: rgba(#fff,.5);
-          }
-        }
-      }
-    }
+var elem = document.getElementsByClassName("range-blue");
+var rangeValue = function (elem, target) {
+	return function (evt) {
+		// target.innerHTML = elem.value;
+    let test = elem.value;
+    colorChange("blue", test);
+	};
+};
+for (var i = 0, max = elem.length; i < max; i++) {
+	bar = elem[i].getElementsByTagName("input")[0];
+	target = elem[i].getElementsByTagName("p")[0];
+	bar.addEventListener("input", rangeValue(bar, target));
+}
+let redy = 100;
+let greeny = 100;
+let bluey = 100;
+let colorChange = (color, value) => {
+  if (color === "red") {
+    let element = document.getElementById("red");
+    element.innerHTML = value;
+    redy = value;
   }
-
-  main {
-    .rgb {
-      .container {
-        margin: 0 auto;
-        width: 80%;
-
-        .range-red {
-          width: 50%;
-        }
-
-        .range-green {
-          width: 50%;
-        }
-
-        .range-blue {
-          width: 50%;
-        }
-
-        .slider {
-          margin: 20px auto;
-          width: 100%;
-          height: 10px;
-          outline: none;
-          border-radius: 5px;
-          background: #d3d3d3;
-          opacity: 1;
-          -webkit-transition: 0.2s;
-          transition: opacity 0.2s;
-
-          -webkit-appearance: none;
-        }
-
-        .range-container {
-          display: flex;
-          flex-direction: column;
-          flex-wrap: wrap;
-          height: 200px;
-
-          .range {
-            width: 300px;
-            background-color: #0ff;
-
-            input {
-              background-color: #00f;
-            }
-
-            span {
-              background-color: #00f;
-            }
-          }
-        }
-
-        .slider::-webkit-slider-thumb {
-          width: 25px;
-          height: 25px;
-          border-radius: 50%;
-          background: #4caf50;
-          cursor: pointer;
-
-          -webkit-appearance: none;
-          appearance: none;
-        }
-
-        .slider::-moz-range-thumb {
-          width: 25px;
-          height: 25px;
-          border-radius: 50%;
-          background: #4caf50;
-          cursor: pointer;
-        }
-
-        .rgb-color-view {
-          width: 300px;
-          height: 100%;
-          background-color: red;
-
-          p {
-            display: inline-block;
-            text-align: center;
-            line-height: 100px;
-          }
-        }
-      }
-    }
+  if (color === "green") {
+    let element = document.getElementById("green");
+    element.innerHTML = value;
+    greeny = value;
   }
+  if (color === "blue") {
+    let element = document.getElementById("blue");
+    element.innerHTML = value;
+    bluey = value;
+  }
+  let colorProp = "rgba(" + redy + "," + greeny + "," + bluey + ",1)";
+  document.getElementsByClassName("rgb-color-view")[0].style.backgroundColor = colorProp;
 }
